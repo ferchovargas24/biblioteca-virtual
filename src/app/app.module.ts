a643b20485b8b2dbf689d12a3f8a492c306b2787
ferva <ferchovargas24@gmail.com>
2017-10-24 16:41:10 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,40 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
+import { FireBase_Config } from './firebase.config';
+import { AngularFireDatabaseModule } from 'angularfire2/database'
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { ListaUsuariosServicio } from './../servicios/lista-usuarios/lista-usuarios.servicio';
+import { MensajeServicio } from '../servicios/mensaje/mensaje.service';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { LoginServicio } from '../servicios/login/login.servicio';
+import { MejoraContraseñaServicio } from '../servicios/mensaje/mejoraContrase\u00F1a';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FireBase_Config),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ListaUsuariosServicio,
+    MensajeServicio,
+    LoginServicio,
+    MejoraContraseñaServicio,
   ]
 })
-export class AppModule {}
+export class AppModule { }
