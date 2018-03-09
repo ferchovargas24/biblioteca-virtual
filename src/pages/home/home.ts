e91ef16d808c674d00039e3b5fa8b5df26fe3c63
ferva <ferchovargas24@gmail.com>
2018-03-09 17:06:04 -0600

    22

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,9 +11,4 @@ export class HomePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
-  }
-
-
 }
