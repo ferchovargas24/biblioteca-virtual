64166a3bae0060c742d5ab1784d44a86057c7d92
ferva <ferchovargas24@gmail.com>
2018-03-14 15:28:29 -0600

    Avanzando

--- src/pages/sesion/sesion.ts
+++ src/pages/sesion/sesion.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import firebase from 'firebase';
 
 @IonicPage()
@@ -10,9 +10,12 @@ import firebase from 'firebase';
 export class SesionPage {
 
   public libros: Array<any> = [];
+  public numeroPedido: number;
+  public nuevaCantidad: number;
   public libroRef: firebase.database.Reference = firebase.database().ref('/libros');
-  public iterador: number;
-  constructor(public navCtrl: NavController, public navParams: NavParams,
+  public pedidoRef: firebase.database.Reference = firebase.database().ref('/pedidos');
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private mensaje: ToastController
   ) {
   }
 
@@ -21,12 +24,13 @@ export class SesionPage {
     this.initializeItems();
   }
 
-  createBook(autor: string, titulo: string, imagen: string): void {
-    this.libroRef.push({ autor, titulo, imagen });
+  createBook(idLibro: number = 0 , autor: string, titulo: string, imagen: string, cantidad: number): void {
+    idLibro +1;
+    this.libroRef.push({ idLibro, autor, titulo, imagen, cantidad });
   }
 
   initializeItems() {
-    
+
     this.libroRef.on('value', libroSnapshot => {
       this.libros = [];
       libroSnapshot.forEach(libroSnap => {
@@ -45,13 +49,35 @@ export class SesionPage {
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
       this.libros = this.libros.filter((item) => {
-        return ( item.autor.toLowerCase().indexOf(
+        return (item.autor.toLowerCase().indexOf(
           val.toLowerCase()) > -1);
       })
     }
   }
 
-  agregarPedido(autorPedido: string){
-    console.log(autorPedido);
+
+  agregarPedido(idLibroPedido: number, autorPedido: string, tituloPedido: string, libroImagenPedido: string, libroCantidad: number) {
+
+      
+      if (libroCantidad > 0 ) {
+
+        this.pedidoRef.push({ autorPedido, tituloPedido, libroImagenPedido }).then(mensaje => {
+          this.mensaje.create({
+            message: 'Se ha guargado tu pedido, ' + tituloPedido + ', recoge tu libro lo antes posible',
+            duration: 2000,
+            position: 'middle'
+          }).present();
+          console.log(libroCantidad);
+          libroCantidad = (libroCantidad - 1);
+          this.libroRef.update({idLibroPedido, libroCantidad});
+        })
+      } else {
+        this.mensaje.create({
+          message: 'No tenemos libros en existencia, intenta mas tarde',
+          duration: 2000,
+          position: 'middle'
+        }).present();
+
+      }
   }
 }
